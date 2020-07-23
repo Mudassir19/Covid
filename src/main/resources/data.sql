3d7bc6e0ffcab0df572bc618daa55fab02f25214
JhanakBhargava <jhanak.bhargava1998@gmail.com>
2020-07-23 19:11:16 +0530

    version1.0

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,10 +5,9 @@ insert into location (id,location) values ('X560002','Bangalore');
 commit;
 
 --USER
-insert into users (id,username,password,active,roles,first_name,last_name,location_id) values('3927985','jhanak.bhargava@xebia.com','password',True,'ROLE_SUPERADMIN','Jhanak','Bhargava','X122413');
-insert into users (id,username,password,active,roles,first_name,last_name,location_id) values('1530815','mudassir.shaikh@xebia.com','password',True,'ROLE_ADMIN','Mudassir','Shaikh','X122413');
-insert into users (id,username,password,active,roles,first_name,last_name,location_id) values('2969368','raghav.sindhwani@xebia.com','password',True,'ROLE_SUPERADMIN','Raghav','Sindhwani','X560002');
-insert into users (id,username,password,active,roles,first_name,last_name,location_id) values('6190459','kamal.kumar@xebia.com','password',True,'ROLE_ADMIN','Kamal','Kumar','X411002');
+insert into users (id,username,password,active,roles,first_name,last_name,location_id) values('3927985','ltiwari@xebia.com','password',True,'ROLE_SUPERADMIN','Laghu','Tiwari','X122413');
+insert into users (id,username,password,active,roles,first_name,last_name,location_id) values('1530815','ndhillon@xebia.com','password',True,'ROLE_ADMIN','Nisha','Dhillon','X122413');
+insert into users (id,username,password,active,roles,first_name,last_name,location_id) values('6190459','pkohli@xebia.com','password',True,'ROLE_ADMIN','Puneet','Kohli','X411002');
 commit;
 
 --STATUS
